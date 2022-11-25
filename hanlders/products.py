6842efd896296ba2179f9bee58ae0bf9b29dc396
-- <yaliyarov@mail.ru>
2022-11-25 19:39:59 +0300

    by Yusif

--- hanlders/products.py
+++ hanlders/products.py
@@ -1,6 +1,38 @@
 def get_product_list(data):
-    pass
+    import json
+    code = 200
+    products = ''
+
+    try:
+        with open('../data/catalog.json', 'r', encoding='utf8') as file:
+            json_file = json.loads(file.read())
+            price = data['filter']['price'][0]
+            category = data['filter']['category']
 
 
-def get_single_product(data):
-    pass
+
+            # Code 400
+            good = False
+            match data:
+                case {'action': _, 'filter': {'price': [_], 'category': cat}}:
+                    if cat in [None, int]:
+                        good = True
+            if not good:
+                code = 400
+                return {'code': code, 'data': products}
+
+            # Main
+            for j in json_file:
+                for prd in j['products']:
+
+                    # Check price and category
+                    if eval(f'{prd["price"]} {price}') and prd['price']:
+                        products += f'{prd["id"]}. {prd["name"]} ({prd["price"]} руб/{prd["unit"]}) {prd["balance"]}шт.\n'
+
+    except:
+        code = 500
+    finally:
+        return {'code': code, 'data': products}
+
+
+print(get_product_list([{"price": ['>=100']}, {"category": None}]))
