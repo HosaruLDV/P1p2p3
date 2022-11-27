513ac65f000ed02c8d87067af7ab9085000c27ca
anna-golovacheva <anna.ch.ollala@gmail.com>
2022-11-28 02:14:24 +0300

    func annotations added, cart.json cleared

--- hanlders/cart.py
+++ hanlders/cart.py
@@ -1,31 +1,59 @@
 import json
 
 
-def get_catalog():
+def get_catalog() -> list:
+    """
+    Функция обрабатывает json-файл и возвращает его содержимое.
+    :return: list
+    """
     with open('data/catalog.json', 'r', encoding='utf-8') as file:
         catalog = json.load(file)
     return catalog
 
 
-def delete_products(pr_id, num):
-    catalog_info = get_catalog()
-    for cat in catalog_info:
-        for product in cat['products']:
-            if product['id'] == pr_id:
-                product['balance'] -= num
-
-    with open('data/catalog.json', 'w', encoding='utf-8') as file:
-        json.dump(catalog_info, file, ensure_ascii=False)
-
-
-def put_product_to_cart(data):
-    print(data)
+def choose_product(data: dict) -> tuple:
+    """
+    Функция позволяет найти запрошенный товар в каталоге.
+    Возвращает содержимое каталога и информацию о выбранном товаре.
+    :param data: dict
+    :return: tuple
+    """
     catalog_info = get_catalog()
     chosen_product = None
     for cat in catalog_info:
         for product in cat['products']:
             if product['id'] == data['data']['id']:
                 chosen_product = product
+    return catalog_info, chosen_product
+
+
+def delete_products(catalog: list, product: dict, num: int) -> None:
+    """
+    Функция уменьшает количество выбранного товара в каталоге.
+    :param catalog: list
+    :param product: dict
+    :param num: int
+    :return: None
+    """
+    product['balance'] -= num
+
+    with open('data/catalog.json', 'w', encoding='utf-8') as file:
+        json.dump(catalog, file, ensure_ascii=False)
+
+
+def put_product_to_cart(data: dict) -> dict:
+    """
+    Функция добавляет выбранный товар в корзину, возвращает сообщение об
+    успешном результате с кодом 201 и информацией о добавленном товаре.
+
+    Если такого товара нет, функция возвращает сообщение об ошибке с кодом 404.
+
+    Если нет необходимого количества выбранного товара, функция возвращает
+    сообщение об ошибке с кодом 409.
+    :param data: dict
+    :return: dict
+    """
+    catalog_info, chosen_product = choose_product(data)
     if not chosen_product:
         return {
             "code": 404,
@@ -46,7 +74,7 @@ def put_product_to_cart(data):
             }
 
         else:
-            delete_products(product_id, product_num)
+            delete_products(catalog_info, chosen_product, product_num)
 
             to_add = [{
                 'product_id': product_id,
@@ -55,15 +83,18 @@ def put_product_to_cart(data):
                 'product_num': product_num,
                 'product_unit': product_unit
             }]
+
             cart_list = []
             with open('data/cart.json', 'r', encoding='utf-8') as file:
                 try:
                     cart_info = json.load(file)
                     if_add_item = True
+
                     for product_in_cart in cart_info:
                         if product_in_cart['product_id'] == to_add[0]['product_id']:
                             product_in_cart['product_num'] += to_add[0]['product_num']
                             if_add_item = False
+
                         cart_list.append(product_in_cart)
 
                     if if_add_item:
@@ -71,6 +102,7 @@ def put_product_to_cart(data):
 
                     with open('data/cart.json', 'w', encoding='utf-8') as file:
                         json.dump(cart_list, file, ensure_ascii=False)
+
                 except ValueError:
                     with open('data/cart.json', 'a', encoding='utf-8') as file:
                         json.dump(to_add, file, ensure_ascii=False)
